47c1e4571125566023e6f7c27fe6c1faf9d22d20
Web Developer / MIS <97711236+GlobaltronicsWebDev@users.noreply.github.com>
2026-06-24 09:21:02 +0800

    changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,6 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet" />
   <link rel="stylesheet" href="{{ asset('cssfolder/mainnavbar.css') }}">
+  <link rel="stylesheet" href="{{ asset('cssfolder/contacts.css') }}" />
   <link rel="stylesheet" href="{{ asset('cssfolder/about.css') }}" />
 </head>
 <body>
