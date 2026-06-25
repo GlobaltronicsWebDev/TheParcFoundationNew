48f8c351d9b372fae9952320ca2b8ae0a67cefce
Web Developer / MIS <97711236+GlobaltronicsWebDev@users.noreply.github.com>
2026-06-25 12:21:08 +0800

    change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,9 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
   <link rel="stylesheet" href="{{ asset('cssfolder/main.css') }}">
-  <link rel="stylesheet" href="{{ asset('cssfolder/mainnavbar.css') }}">
-  <link rel="stylesheet" href="{{ asset('cssfolder/contacts.css') }}" />
-  <link rel="stylesheet" href="{{ asset('cssfolder/carousel.css') }}" />
+  <link rel="stylesheet" href="{{ asset('cssfolder/mainnavbar.css?v=2R') }}">
+  <link rel="stylesheet" href="{{ asset('cssfolder/contacts.css?v=2R') }}" />
+  <link rel="stylesheet" href="{{ asset('cssfolder/carousel.css?v=2R') }}" />
 
 
 </head>
