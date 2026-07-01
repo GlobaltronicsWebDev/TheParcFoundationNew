753add66fd222f237960a4ff4c5d950717c0a624
Web Developer / MIS <97711236+GlobaltronicsWebDev@users.noreply.github.com>
2026-07-01 16:24:30 +0800

    fileformat

--- resources/views/layouts/carousel.blade.php
+++ resources/views/layouts/carousel.blade.php
@@ -18,7 +18,7 @@
             <img src="{{ asset('assets/image/sliders/slider4.jpg') }}" class="d-block w-100" alt="Slide 4">
           </div>
           <div class="carousel-item">
-            <img src="{{ asset('assets/image/sliders/slider6.png') }}" class="d-block w-100" alt="Slide 5">
+            <img src="{{ asset('assets/image/sliders/slider6.jpg') }}" class="d-block w-100" alt="Slide 5">
           </div>
            
         </div>
